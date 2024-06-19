814b77b5cd15dfaafb8560b4e01be5f164a76d6b
Felipe Alves <felipe.contato.a@gmail.com>
2024-06-19 07:38:51 -0300

    fix(app/register): 🚑Regexp da regra de deve conter letras na senha estava errado - \w inclui números também

--- src/app/[lang]/register/_providers/Registration.tsx
+++ src/app/[lang]/register/_providers/Registration.tsx
@@ -11,7 +11,7 @@ const passwordValidationRules = {
     length: (value: string) => value.length >= PASSWORD_MIN_LENGTH,
     specialChars: (value: string) => /[!@#$%^&*(),.?":{}|<>]/g.test(value),
     numbers: (value: string) => /\d/g.test(value),
-    letters: (value: string) => /\w/g.test(value),
+    letters: (value: string) => /[a-zA-Z]/g.test(value),
     uppercase: (value: string) => /[A-Z]/g.test(value),
     lowercase: (value: string) => /[a-z]/g.test(value),
 };
