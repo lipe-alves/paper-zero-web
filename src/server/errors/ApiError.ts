5eddfa4219dd06dc822fa6da8595aa0238e1dd74
Felipe Alves <felipe.contato.a@gmail.com>
2024-06-19 16:11:15 -0300

    fix(server/errors/ApiError): 🚑 ApiError.message ficando omitido no retorno para o cliente

--- src/server/errors/ApiError.ts
+++ src/server/errors/ApiError.ts
@@ -8,18 +8,19 @@ interface ApiErrorParams<T> {
     data: T;
 }
 
-class ApiError<T> extends Error implements ResponseFormat<T> {
+class ApiError<T> implements ResponseFormat<T> {
     public readonly success: false;
     public status: number;
+    public message: string;
     public code: ResponseCode;
     public data: T;
 
     constructor(params: ApiErrorParams<T>) {
         const { message, status, code, data } = params;
-        super(message);
 
         this.success = false;
         this.status = status;
+        this.message = message;
         this.code = code;
         this.data = data;
     }
@@ -29,7 +30,7 @@ class ApiError<T> extends Error implements ResponseFormat<T> {
             status: error.status || 500,
             code: error.code || RESPONSE_CODES.INTERNAL_SERVER_ERROR,
             message: error.message || "Erro Interno do Servidor",
-            data: error,
+            data: error.data || error,
         });
     }
 }
